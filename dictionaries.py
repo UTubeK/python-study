7c038de07c58638f30c169557025f82bd99e11af
UTubeK <khanho.lee55@gmail.com>
2023-01-04 20:52:17 -0500

    dictionaries lesson

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+ice_cream = {'Alice': 'chocolate', 'Bob': 'strawberry', 'Cara': 'mint chocolate chip'}
+print(ice_cream['Alice'])
+print(ice_cream['Bob'])
+ice_cream['Eve'] = 'lemonade'
+print(ice_cream)
+print('Eve' in ice_cream)
+ice_cream['Alice'] = 'vanilla'
+print(ice_cream)
+job = {}
+print(type(job))
